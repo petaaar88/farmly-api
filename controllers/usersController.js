d78bf55c2733f4f5aad2498881549134f685ccd7
petar <pe.bn80@gmail.com>
2026-02-01 20:51:39 +0100

    Fix GET /api/users/{userId}/profile to return correct response

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -13,13 +13,7 @@ const registerUser = async (req, res, next) => {
 
 const getUserProfileHandler = async (req, res, next) => {
     const user = await getUserProfile(parseInt(req.params.userId));
-    res.status(200).json({
-        fullName: user.fullName,
-        email: user.email,
-        city: user.city,
-        phoneNumber: user.phoneNumber,
-        description: user.description
-    });
+    res.status(200).json(user);
 };
 
 export { registerUser, getUserProfileHandler };
